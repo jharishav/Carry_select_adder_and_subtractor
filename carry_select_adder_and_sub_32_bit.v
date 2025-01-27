8221be6e7da3525aa0bfe1016cc9be4fffb6cc21
Rishav Kumar <35143365+jharishav@users.noreply.github.com>
2025-01-27 19:40:06 +0530

    Add files via upload

--- carry_select_adder_and_sub_32_bit.v
+++ carry_select_adder_and_sub_32_bit.v
@@ -20,25 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module carry_select_adder_and_sub_32_bit(a,b_xor,cin,sum_out,carry_out);
-input [31:0] a, b_xor;
+module carry_select_adder_and_sub_32_bit(a,b,cin,sum_out);
+input [31:0] a, b;
 input cin;
 output[31:0] sum_out;
-output carry_out;
+
 
 wire [32:0] carry_propagate;
-wire [31:0] b;
+wire [31:0] b_xor;
 
 assign carry_propagate[0] = cin;
-assign b = b_xor ^ {32{cin}};
 
 genvar j;
 
 generate for(j=0;j<=31;j=j+1) begin: add
 
-carry_select_adder_1bit a1(a[j],b[j],carry_propagate[j],sum_out[j],carry_propagate[j+1]);
+assign b_xor[j] = b[j] ^ cin; 
+carry_select_adder_1bit a1(a[j],b_xor[j],carry_propagate[j],sum_out[j],carry_propagate[j+1]);
 end
 endgenerate
 
-assign carry_propagate[32] = carry_out;
+
 endmodule

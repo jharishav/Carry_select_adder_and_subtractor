308410cb701f0412a9f4d49c1fe750c5c6d9bade
Rishav Kumar <35143365+jharishav@users.noreply.github.com>
2025-01-27 19:37:20 +0530

    Updated test bench using $random
    In this updated test bench module, I have used system task $random to generate random stimulus and tested DUT

--- carry_select_adder_tb.v
+++ carry_select_adder_tb.v
@@ -24,17 +24,25 @@ module carry_select_adder_tb();
 reg [31:0] A, B;
 reg C_IN;
 wire [31:0] SUM;
-wire C_OUT;
+reg [31:0] CH_SUM;
+reg DIFF;
+integer i;
 
-carry_select_adder_and_sub_32_bit uut(A,B,C_IN,SUM,C_OUT);
+carry_select_adder_and_sub_32_bit uut(A,B,C_IN,SUM);
 
 initial
 begin
-A = 32'd9; B = 32'd5; C_IN = 1'b0;
-#20 A = 32'd900; B = 32'd90; C_IN = 1'b1;
-#20 A = 32'd14; B = 32'd13; C_IN = 1'b1;
-#20 A = 32'd200; B = 32'd155; C_IN = 1'b1;
-#20 A = 32'd265; B = 32'd225; C_IN = 1'b0;
-#20 $finish;
-end
+
+for (i=0;i<=50;i=i+1)
+begin
+    A = $random;
+    B = $random;
+    C_IN = $random;
+    if (C_IN) CH_SUM = A + (~B) + C_IN;
+    else CH_SUM = A + B; 
+    assign DIFF = CH_SUM - SUM;  #10;
+    end 
+    
+    #510 $finish;
+   end
 endmodule
